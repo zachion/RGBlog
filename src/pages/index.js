de9dba325b182639267df9693a691a1bb537a322
Zach Ioannou <zioannou77@gmail.com>
2020-01-27 22:45:14 +0200

    first use of Link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello</h1>
       <h2>I'm Zach, a full-stack developer living in beautiful Athens</h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me</Link>
+      </p>
     </div>
   )
 }
