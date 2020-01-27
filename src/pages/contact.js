12f66c83af639e82f8fd0c8d4acb58e331aa0c7d
Zach Ioannou <zioannou77@gmail.com>
2020-01-27 22:39:07 +0200

    added more pages

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>I speak hex, ascii and binary, alternatively find me on twitter.</p>
+    </div>
+  )
+}
+
+export default ContactPage
