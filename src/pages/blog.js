04675fdef68178c801a0b1598ff138f93a740f6a
Zach Ioannou <zioannou77@gmail.com>
2020-01-27 22:31:10 +0200

    creating first new pages

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <p>Post will show here later on.</p>
+    </div>
+  )
+}
+export default BlogPage
