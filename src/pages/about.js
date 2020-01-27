f71fe31441192a5a798c325242c352e5456f5805
Zach Ioannou <zioannou77@gmail.com>
2020-01-27 22:51:03 +0200

    playing with link component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About Me</h1>
-      <p>This is the about page</p>
+      <p>This is the about page.</p>
+      <Link to="/contact">Contact</Link>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>I speak hex, ascii and binary, alternatively find me on twitter.</p>
+      <p>
+        I speak hex, ascii and binary, alternatively find me on{" "}
+        <Link to="https://twitter.com/ha77is" >twitter</Link>
+      </p>
     </div>
   )
 }
